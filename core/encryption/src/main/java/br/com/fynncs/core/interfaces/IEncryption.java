980185c228a913e5c19d825bb108e385abf129c4
Miqueias <miqueiasnadaluti5@gmail.com>
2024-12-26 18:14:55 -0300

    Create Encryption

---
+++ core/encryption/src/main/java/br/com/fynncs/core/interfaces/IEncryption.java
@@ -0,0 +1,14 @@
+package br.com.fynncs.core.interfaces;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+public interface IEncryption {
+    public String encrypt(String text, String key) throws InvalidKeySpecException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException;
+    public String decrypt(String text, String key) throws Exception;
+}
